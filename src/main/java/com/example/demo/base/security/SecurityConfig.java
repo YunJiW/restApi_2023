f98ac064ff8d1f77f8f6f0eaedffb43bb59530a4
YunJiW <dtroy371@gmail.com>
2023-08-17 15:56:39 +0900

    feat : SpringSecurity에서 필요없는 설정 ë끄기

---
+++ src/main/java/com/example/demo/base/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo.base.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .cors(cors->cors.disable()) // 타도메인에서 APi 호출가능
+                .csrf(csrf -> csrf.disable()) // CSRF 토큰 끄기
+                .httpBasic(httpBasic -> httpBasic.disable()) //httpBasic 로그인 방법끄기
+                .formLogin(formLogin -> formLogin.disable()) // 폼로그인 방식 끄기
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );//세션을 쓰지 않겠다.
+
+
+
+        return http.build();
+    }
+}
